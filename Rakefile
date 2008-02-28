45878b3c8af8a50464c35cb232d873e2c70d5551
David Fayram <dfayram@gmail.com>
2008-02-27 22:04:57 -0800

    A bit more work. Nearly ready to start.

--- Rakefile
+++ Rakefile
@@ -5,6 +5,11 @@ task :default do
   sh "mzc -z -d lib/ src/*.scm"
 end
 
+task :console do
+  puts "Erlenmeyer Console - All Modules Loaded\n"
+  exec "mzscheme -m -S ./lib/ -t lib/*.zo"
+end
+
 task :clean do
   sh "git clean -xf"
 end
--- src/erlenmeyer.scm
+++ src/erlenmeyer.scm
@@ -1,7 +1,17 @@
 (module erlenmeyer mzscheme
   (require (lib "foreign.ss")) (unsafe!)
-  (provide fd->input-port
+  (provide bind-erlang-ports
+           fd->input-port
            fd->output-port)
+
+;  We'll start with the standard ports
+  (define erlang-input-port (current-input-port))
+  (define erlang-output-port (current-output-port))
+ 
+ (define (bind-erlang-ports)
+   (set! erlang-input-port  (fd->input-port 3 'erlang-in))
+   (set! erlang-output-port (fd->output-port 4 'erlang-out)))
+  
 ; The all-important fd->input port code thanks to Matthew Flatt
   (define (fd->input-port fd name)
     (scheme_make_fd_input_port fd name 0 0))
